de4803bb8f68f75db81fbce294757ed85e4b829c
Bich Le <leb@platform9.net>
2017-03-14 15:18:25 -0700

    Add topic support

--- fedDemo/s2n-builder/fetch_and_build.sh
+++ fedDemo/s2n-builder/fetch_and_build.sh
@@ -3,10 +3,16 @@
 while true
 do
     echo Fetching message ...
-    if curl -fk https://mq.kube2go.io/v1/messages ; then
+    if curl -fsS https://mq.kube2go.io/v1/topics/requests/messages ; then
         echo Building ...
-        time bash -c 'rm -rf s2n && git clone --depth 1 https://github.com/awslabs/s2n.git && pushd s2n && make bin && popd'
-        echo Done ...
+        if rm -rf s2n && git clone --depth 1 https://github.com/awslabs/s2n.git && pushd s2n && make bin && popd; then
+            echo build succeeded
+            curl -fsS -X POST https://mq.kube2go.io/v1/topics/success/messages
+        else
+            echo build failed
+            curl -fsS -X POST https://mq.kube2go.io/v1/topics/failure/messages
+        fi
+
     else
         echo Failed to get message
         sleep 1
--- ridiculouslySimpleMQ/api.js
+++ ridiculouslySimpleMQ/api.js
@@ -17,10 +17,17 @@ const TIMEOUT = process.env.TIMEOUT || 10000;
 const LISTEN_PORT = process.env.LISTEN_PORT || 8889;
 const LOG_LEVEL = process.env.LOG_LEVEL || 'INFO';
 
-const messages = [];
-const waiters = [];
+const messages = {};
+const waiters = {};
 
 start();
+
+function ensureTopic(topic) {
+    if (typeof(messages[topic]) === 'undefined')
+        messages[topic] = [];
+    if (typeof(waiters[topic]) === 'undefined')
+        waiters[topic] = [];
+}
 
 function apiLogger(log) {
     return co.wrap(function *(ctx, next) {
@@ -33,8 +40,8 @@ function apiLogger(log) {
             let errMsg = '';
             if (ctx.status >= 400 && ctx.message)
                 errMsg = '- ' + ctx.message;
-            const nm = messages.length;
-            const nw = waiters.length;
+            const nm = messages.requests? messages.requests.length : -1;
+            const nw = waiters.requests? waiters.requests.length : -1;
             log.info(`${ctx.method} ${ctx.url} ${ctx.status} - ${ms}ms ${errMsg} - ${nm} msgs ${nw} waiters`);
         } catch(error) {
             ctx.status = ctx.status || 500;
@@ -48,38 +55,39 @@ function start() {
     log.setLevel(LOG_LEVEL);
     app.use(apiLogger(log));
     app.use(bodyParser());
-    app.use(route.post('/v1/messages', co.wrap(genPublishMessage)));
-    app.use(route.get('/v1/messages', co.wrap(genPullMessage)));
-    app.use(route.get('/v1/messageCount', co.wrap(genGetMessageCount)));
+    app.use(route.post('/v1/topics/:topic/messages', co.wrap(genPublishMessage)));
+    app.use(route.get('/v1/topics/:topic/messages', co.wrap(genPullMessage)));
+    app.use(route.get('/v1/topics/:topic/messageCount', co.wrap(genGetMessageCount)));
     app.listen(LISTEN_PORT);
 }
 
-function * genPublishMessage(ctx) {
+function * genPublishMessage(ctx, topic) {
     log.debug('body:', ctx.request.body);
-    if (waiters.length) {
+    ensureTopic(topic);
+    if (waiters[topic].length) {
         log.debug('dequeueing a waiter');
-        const emitter = waiters.shift();
+        const emitter = waiters[topic].shift();
         emitter.emit('msg', ctx.request.body);
     } else {
-        messages.push(ctx.request.body);
+        messages[topic].push(ctx.request.body);
     }
     ctx.status = 200;
 }
 
-function * genWaitForMessage() {
+function * genWaitForMessage(topic) {
     const emitter = new EventEmitter();
-    waiters.push(emitter);
+    waiters[topic].push(emitter);
     const prom = utils.withTimeout(new Promise(promFn), TIMEOUT);
     try {
         return yield * utils.wrapPromiseError(prom);
     } catch (e) {
         // timeout
-        const i = waiters.indexOf(emitter);
+        const i = waiters[topic].indexOf(emitter);
         if (i < 0)
             log.warn('Unable to remove waiter after timeout');
         else {
             log.debug('Removing waiter after timeout', i);
-            waiters.splice(i, 1);
+            waiters[topic].splice(i, 1);
         }
         emitter.removeAllListeners();
         throw e;
@@ -90,19 +98,21 @@ function * genWaitForMessage() {
     }
 }
 
-function * genPullMessage(ctx) {
-    if (messages.length) {
-        ctx.body = messages.shift();
+function * genPullMessage(ctx, topic) {
+    ensureTopic(topic);
+    if (messages[topic].length) {
+        ctx.body = messages[topic].shift();
     } else try {
-        ctx.body = yield * genWaitForMessage();
+        ctx.body = yield * genWaitForMessage(topic);
     } catch (e) {
         log.debug('genPullMessage error:', e.toString());
         ctx.status = 404;
     }
 }
 
-function *genGetMessageCount(ctx) {
-    ctx.body = messages.length;
+function *genGetMessageCount(ctx, topic) {
+    ensureTopic(topic);
+    ctx.body = messages[topic].length;
     ctx.dontLog = true;
 }
 
