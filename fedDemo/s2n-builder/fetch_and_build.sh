d053656e724e77f418b938657046c1320bc3506d
Bich Le <leb@platform9.net>
2017-03-16 14:41:14 -0700

    Add time stamps to builder script

--- fedDemo/s2n-builder/fetch_and_build.sh
+++ fedDemo/s2n-builder/fetch_and_build.sh
@@ -4,12 +4,12 @@ while true
 do
     echo Fetching message ...
     if curl -fsS https://mq.kube2go.io/v1/topics/requests/messages ; then
-        echo Building ...
+        echo Building started at `date`
         if rm -rf s2n && git clone --depth 1 https://github.com/awslabs/s2n.git && pushd s2n && make bin && popd; then
-            echo build succeeded
+            echo build succeeded at `date`
             curl -fsS -X POST https://mq.kube2go.io/v1/topics/success/messages
         else
-            echo build failed
+            echo build failed at `date`
             curl -fsS -X POST https://mq.kube2go.io/v1/topics/failure/messages
         fi
 
